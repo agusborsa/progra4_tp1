ac249b927678f1d5ab2a67d5655033cc911187a7
Agustin <agustinborsaumai@gmail.com>
2024-03-24 21:16:11 -0300

    fixing type modules for app.ts

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
 type Usuario =
-    | { tipo: "persona", persona: Persona }
-    | { tipo: "tareas", tareas: Tarea}
+    { tipo: "usuario", persona: Persona, tarea: Tarea }
 
-type Tarea =
+/* type Tarea =
+    | { tipo: "estado", estado: string  }
     | { tipo: "prioridad", prioridad: string  }
     | { tipo: "fecha", fecha: Date }
-    | { tipo: "descripcion", opcion: string }
+    | { tipo: "descripcion", opcion: string } */
 
 interface Persona {
     nombre: string,
     apellido?: string
 }
+
+interface Tarea {
+    estado: string,
+    prioridad: string,
+    fecha: Date,
+    descripcion?: string
+}
+
